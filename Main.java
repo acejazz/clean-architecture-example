6df1f6ced3936e2f3d0d22fcb6d693dbc48e0086
acejazz <taniomessina@gmail.com>
2019-09-22 22:12:30 +0100

    Add class diagram and cleanup Main (#1)

--- Main.java
+++ Main.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Main{
 
     public static void main(String args[]){
+        
         InputReader inputReader = new ScannerInputReader();
+        
         OutputDisplayer outputDisplayer = new ConsoleOutputDisplayer();
-
         CapitalizeLettersOutputPort presenter = new ConsolePrinterPresenter(outputDisplayer);
         CapitalizeLettersInputPort capitalizeLettersUseCase = new CapitalizeLettersUseCase(presenter);
 
@@ -18,6 +19,57 @@ public class Main{
     }
 }
 
+
+// Input
+interface InputReader{
+    String getInput();
+}
+
+class ScannerInputReader implements InputReader{
+
+    private Scanner scanner;
+
+    public ScannerInputReader(){
+        scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public String getInput(){
+        return scanner.next();
+    }
+}
+
+// Output
+interface OutputDisplayer{
+    void display(String output);
+}
+
+class ConsoleOutputDisplayer implements OutputDisplayer{
+
+    @Override
+    public void display(String output){
+        System.out.println(output);
+    }
+}
+
+// Controller
+class CapitalizeLettersController{
+
+    private CapitalizeLettersInputPort useCaseInputPort;
+    private InputReader inputReader;
+
+    public CapitalizeLettersController(CapitalizeLettersInputPort useCaseInputPort, InputReader inputReader){
+        this.useCaseInputPort = useCaseInputPort;
+        this.inputReader = inputReader;
+    }
+
+    public void capitalizeLetters(){
+        String word = inputReader.getInput();
+        useCaseInputPort.capitalizeLetters(word);
+    }
+}
+
+// Ports
 interface CapitalizeLettersInputPort{
     void capitalizeLetters(String input);
 }
@@ -41,27 +93,6 @@ class CapitalizeLettersUseCase implements CapitalizeLettersInputPort{
     }
 }
 
-
-interface InputReader{
-    String getInput();
-}
-
-class CapitalizeLettersController{
-
-    private CapitalizeLettersInputPort useCaseInputPort;
-    private InputReader inputReader;
-
-    public CapitalizeLettersController(CapitalizeLettersInputPort useCaseInputPort, InputReader inputReader){
-        this.useCaseInputPort = useCaseInputPort;
-        this.inputReader = inputReader;
-    }
-
-    public void capitalizeLetters(){
-        String word = inputReader.getInput();
-        useCaseInputPort.capitalizeLetters(word);
-    }
-}
-
 class ConsolePrinterPresenter implements CapitalizeLettersOutputPort{
 
     private OutputDisplayer displayer;
@@ -73,32 +104,5 @@ class ConsolePrinterPresenter implements CapitalizeLettersOutputPort{
     @Override
     public void display(String output){
         displayer.display(output);
-    }
-}
-
-
-class ScannerInputReader implements InputReader{
-
-    private Scanner scanner;
-
-    public ScannerInputReader(){
-        scanner = new Scanner(System.in);
-    }
-
-    @Override
-    public String getInput(){
-        return scanner.next();
-    }
-}
-
-interface OutputDisplayer{
-    void display(String output);
-}
-
-class ConsoleOutputDisplayer implements OutputDisplayer{
-
-    @Override
-    public void display(String output){
-        System.out.println(output);
     }
 }
